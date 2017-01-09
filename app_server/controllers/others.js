61cee0f22d531497de5375b701bc25ee8b131dae
toashel <shelchean@gmail.com>
2017-01-09 14:19:07 +1300

    Add angular app

---
+++ app_server/controllers/others.js
@@ -0,0 +1,6 @@
+// Get Angular SPA page //
+module.exports.angularApp = function(req, res) {
+	res.render('layout', {title: 'Kisetsu'});
+};
+
+// Get About Page?
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AniVersus' });
-});
+router.get('/', ctrlOthers.angularApp);
+
+// Catch all
+router.get('*', ctrlOthers.angularApp);
 
 module.exports = router;
