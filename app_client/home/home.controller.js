44eecef58f4cd8206090b2f25df0652bf9fb886a
toashel <shelchean@gmail.com>
2017-01-10 22:26:40 +1300

    Add swal form and custom radio buttons

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -13,8 +13,22 @@ function homeCtrl (anidata) {
 		// console.log(vm.anime);
 	});
 
-	vm.testId = function(id) {
-		console.log(id);
+	vm.testId = function(id, name) {
+		swal({
+			title: 'Rating',
+			text: 'What did you think about ' + name + ' this week?',
+			type: 'question',
+			confirmButtonText: 'Go!',
+			input: 'radio',
+			inputOptions: {
+				1: '<i class="material-icons md-48" data-toggle="tooltip" title="It was pretty bad">sentiment_very_dissatisfied</i>',
+				2: '<i class="material-icons md-48">sentiment_dissatisfied</i>',
+				3: '<i class="material-icons md-48">sentiment_neutral</i>',
+				4: '<i class="material-icons md-48">sentiment_satisfied</i>',
+				5: '<i class="material-icons md-48">sentiment_very_satisfied</i>'
+			}
+
+		});
 	};
 
 }
